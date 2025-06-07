0915c51b53f53297626af6646115e144c94f3292
Benfranklin m s <bfranklinms@gmail.com>
2025-06-07 15:20:08 +0530

    implement Admin component with task creation form

--- src/components/Dashboard/Admin.jsx
+++ src/components/Dashboard/Admin.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from '../Other/Header'
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className='h-screen w-full p-10'>
+      <Header/>
+      <div>
+        <form className='flex items-start justify-between '>
+          <h3>Task Title</h3>
+          <input type="text" placeholder='Make a ui design'/>
+          <h3>Task Description</h3>
+          <textarea></textarea>
+          <h3>Date</h3>
+          <input type="date"/>
+          <h3>Assign to</h3>
+          <input type="text" placeholder='Employee name'/>
+          <h3>Category</h3>
+          <input type="text" placeholder='design, dev, etc'/>
+          <button>Create task</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
